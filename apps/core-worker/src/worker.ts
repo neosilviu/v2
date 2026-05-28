409c1732f8c855f7ef2995fa35a87aa7e09687da
Silviu-Marian Ilie <neosilviu@gmail.com>
2026-05-28 23:40:25 +0300

    feat: mount generic runtime registry routes

--- apps/core-worker/src/worker.ts
+++ apps/core-worker/src/worker.ts
@@ -1,6 +1,8 @@
 import app from "./index";
 import { createSurfaceRoutes } from "./surface-routes";
+import { createRuntimeRegistryRoutes } from "./runtime-registry-routes";
 
 app.route("/runtime/ui", createSurfaceRoutes());
+app.route("/", createRuntimeRegistryRoutes());
 
 export default app;
