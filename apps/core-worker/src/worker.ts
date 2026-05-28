ea808e6c660c24b3d289f0903ae1cb84949cb141
Silviu-Marian Ilie <neosilviu@gmail.com>
2026-05-29 00:04:17 +0300

    refactor: mount compact platform settings routes

--- apps/core-worker/src/worker.ts
+++ apps/core-worker/src/worker.ts
@@ -1,8 +1,10 @@
 import app from "./index";
+import { createPlatformSettingsRoutes } from "./platform-settings-routes";
 import { createSurfaceRoutes } from "./surface-routes";
 import { createRuntimeRegistryRoutes } from "./runtime-registry-routes";
 
 app.route("/runtime/ui", createSurfaceRoutes());
+app.route("/", createPlatformSettingsRoutes());
 app.route("/", createRuntimeRegistryRoutes());
 
 export default app;
